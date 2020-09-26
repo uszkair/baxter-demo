af7f20885a4fd5b7386ec405cc369b0f4182be1e
uszkair <uszkaimail@gmail.com>
2020-09-26 23:53:40 +0200

    táblázat rendezés, lapozás

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {APP_INITIALIZER, forwardRef, NgModule} from '@angular/core';
+import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatOptionModule } from '@angular/material/core';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import {FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatOptionModule} from '@angular/material/core';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatTableModule} from '@angular/material/table';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
@@ -26,16 +26,17 @@ import {AdminComponent} from './secured/admin/admin.component';
 import {AuthInterceptor} from './shared/interceptors/auth-interceptor';
 import {DashboardComponent} from './secured/dashboard/dashboard.component';
 import {BasicHttpInterceptor} from './shared/interceptors/basic-http.interceptor';
-import { LeftSideNavComponent } from './secured/left-side-nav/left-side-nav.component';
-import { HouseListComponent } from './secured/house-list/house-list.component';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
-import { NewHouseComponent } from './secured/new-house/new-house.component';
+import {LeftSideNavComponent} from './secured/left-side-nav/left-side-nav.component';
+import {HouseListComponent} from './secured/house-list/house-list.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {NewHouseComponent} from './secured/new-house/new-house.component';
 import {MatStepperModule} from '@angular/material/stepper';
 import {TextMaskModule} from 'angular2-text-mask';
 import {MatCheckboxModule} from "@angular/material/checkbox";
-import { TaxNumComponent } from './shared/tax-num/tax-num.component';
-import {MatFormFieldControl} from "@angular/material/form-field";
-import { BankAccountComponent } from './shared/bank-account/bank-account.component';
+import {TaxNumComponent} from './shared/tax-num/tax-num.component';
+import {BankAccountComponent} from './shared/bank-account/bank-account.component';
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatSortModule} from "@angular/material/sort";
 
 
 @NgModule({
@@ -60,6 +61,8 @@ import { BankAccountComponent } from './shared/bank-account/bank-account.compone
     FormsModule,
     ReactiveFormsModule,
     MatToolbarModule,
+    MatPaginatorModule,
+    MatSortModule,
     MatInputModule,
     MatCardModule,
     MatMenuModule,
--- src/app/secured/house-list/house-list.component.ts
+++ src/app/secured/house-list/house-list.component.ts
@@ -1,21 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {HouseListService} from './house-list.service';
 import {House} from '../../models/House';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatDialog} from "@angular/material/dialog";
 import {NewHouseComponent} from "../new-house/new-house.component";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-house-list',
   templateUrl: './house-list.component.html',
   styleUrls: ['./house-list.component.scss']
 })
-export class HouseListComponent implements OnInit {
+export class HouseListComponent implements OnInit, AfterViewInit {
   public displayedColumns = ['houseName', 'postCode', 'strata'];
   public dataSource = new MatTableDataSource<any>();
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor(private houseService: HouseListService,
               public dialog: MatDialog) {
+  }
+
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   ngOnInit() {
