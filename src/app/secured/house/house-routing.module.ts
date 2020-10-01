e860c75bdec9b082b46345f6ce6012ea620179af
uszkair <uszkaimail@gmail.com>
2020-10-01 22:56:11 +0200

    sharedmodule

--- src/app/secured/house/house-routing.module.ts
+++ src/app/secured/house/house-routing.module.ts
@@ -1,8 +1,8 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {AuthGuardService} from "../../shared/services/auth/auth-guard.service";
-import {HouseDashboardComponent} from "./house-dashboard/house-dashboard.component";
-import {HouseOverviewComponent} from "./overview/overview.component";
+import {AuthGuardService} from '../../shared/services/auth/auth-guard.service';
+import {HouseDashboardComponent} from './house-dashboard/house-dashboard.component';
+import {HouseOverviewComponent} from './overview/overview.component';
 
 const routes: Routes = [
   { path: 'overview', component: HouseOverviewComponent, outlet: 'sidebar' , canActivate: [ AuthGuardService ] },
